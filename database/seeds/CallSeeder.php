cbfce83d7167d819ef05b88e0ea4e1d3ddfcb7ef
masoud vafaei <masoudvafaei@gmail.com>
2020-07-21 00:20:30 +0430

    models

---
+++ database/seeds/CallSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CallSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $count = 100;
+        factory(\App\calls::class, $count)->create();
+    }
+}
